83384188804b86baea7465c8f4be2142f86b799e
Klalvarezm <Klalvarezm@users.noreply.github.com>
2022-05-10 15:58:02 +0000

    deploy: b2d8ddaf73445bf6ee86f96f3269d15f15aedaf0

--- docs/shortcodes/Taller2/sketch.js
+++ docs/shortcodes/Taller2/sketch.js
@@ -1,30 +1,12 @@
 /*
-FACE DETECTION: SIMPLE
+Eye Tracking Tomado de:
 Jeff Thompson | 2021 | jeffreythompson.org
 
-Teaching computers to see faces has been around in 
-various forms since the 1960s. In the 1990s and early 
-2000s, techniques like Eigenfaces and Viola-Jones
-allowed not just detecting faces but identifying
-features. Today, facial recognition is embedded in
-our smartphones and social media apps, allowing for
-super fast, accurate, and even 3D face tracking!
-
-There are lots of libraries for doing face detection,
-but Google's TensorFlow library has several models
-that work really well. This example uses BlazeFace,
-which tracks six features and is really fast!
-
-MORE INFO
+Referencias
 + https://github.com/tensorflow/tfjs-models/tree/master/blazeface
 + https://arxiv.org/abs/1907.05047
 
-CHALLENGES
-1. What other info does BlazeFace provide us? (Hint:
-   look in the console)
-2. In this example, the eyes are always drawn at the
-   same size. Can you make them change size based
-   on the face's overall size?
+
 
 */
 
@@ -38,9 +20,10 @@ let firstFace = true;
 
 
 function setup() {
-  var canvas=createCanvas(640, 480);
+  var canvas=createCanvas(400, 380,WEBGL);
   canvas.parent('sketch-holder');
   video = createCapture(VIDEO);
+  video.size(300,300)
   video.hide();
 
   // load the BlazeFace model
@@ -58,7 +41,8 @@ async function loadFaceModel() {
 
 
 function draw() {
-
+  scale(-1, 1);
+  background(200);
   // if the video is active and the model has
   // been loaded, get the face from this frame
   if (video.loadedmetadata && model !== undefined) {
@@ -67,7 +51,7 @@ function draw() {
 
   // if we have face data, display it
   if (face !== undefined) {
-    image(video, 0,0, width,height);
+    image(video, -200,-170, width,height);
 
     // if this is the first face we've
     // found, print the info
@@ -80,31 +64,21 @@ function draw() {
     // (see the output in the console) but the
     // most useful will probably be landmarks,
     // which correspond to facial features
-    let rightEye = face.landmarks[0];
     let leftEye =  face.landmarks[1];
-    let nose =     face.landmarks[2];
-    let rightEar = face.landmarks[4];
-    let leftEar =  face.landmarks[5];
-
     // the points are given based on the dimensions
     // of the video, which may be different than
     // your canvas – we can convert them using map()!
-    rightEye = scalePoint(rightEye);
     leftEye =  scalePoint(leftEye);
-    nose =     scalePoint(nose);
+   
 
     // from there, it's up to you to do fun
     // stuff with those points!
-    fill(255);
-    noStroke();
-    circle(leftEye.x,  leftEye.y,  40);     // eyeball
-    circle(rightEye.x, rightEye.y, 40);
-    fill(0);
-    circle(leftEye.x,  leftEye.y,  15);     // pupil
-    circle(rightEye.x, rightEye.y, 15);
-    fill(220);
-    circle(leftEye.x+2,  leftEye.y-2,  5);  // reflection
-    circle(rightEye.x+2, rightEye.y-2, 5);
+    push();// begin motion
+    translate(leftEye.x-450,leftEye.y-300,leftEye.z+3);
+    fill(0,220,210);
+    box(20,20);
+    pop();//end motion
+    
   }
 }
 
