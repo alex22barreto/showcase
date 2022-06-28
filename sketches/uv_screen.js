8536b3a631e53b8b3c840f0e7716bcf266374636
alex22barreto <alex22barreto@users.noreply.github.com>
2022-06-28 05:13:22 +0000

    deploy: 4edb4ad18bcf510a2cddc5e777f484b82291a5ae

--- sketches/uv_screen.js
+++ sketches/uv_screen.js
@@ -3,11 +3,6 @@ let uvShader;
 let opacity;
 
 function preload() {
-  // Define geometry in world space (i.e., matrices: Tree.pmvMatrix).
-  // The projection and modelview matrices may be emitted separately
-  // (i.e., matrices: Tree.pMatrix | Tree.mvMatrix), which actually
-  // leads to the same gl_Position result.
-  // Interpolate only texture coordinates (i.e., varyings: Tree.texcoords2).
   // see: https://github.com/VisualComputing/p5.treegl#handling
   uvShader = readShader('/showcase/sketches/shaders/uv_alpha.frag', { matrices: Tree.pmvMatrix, varyings: Tree.texcoords2 });
 }
@@ -16,13 +11,13 @@ function setup() {
   createCanvas(300, 300, WEBGL);
   // easycam stuff
   let state = {
-    distance: 250,           // scalar
-    center: [0, 0, 0],       // vector
-    rotation: [0, 0, 0, 1],  // quaternion
+    distance: 250,           
+    center: [0, 0, 0],       
+    rotation: [0, 0, 0, 1],  
   };
   easycam = createEasyCam();
-  easycam.state_reset = state;   // state to use on reset (double-click/tap)
-  easycam.setState(state, 2000); // now animate to that state
+  easycam.state_reset = state;   
+  easycam.setState(state, 2000); 
   textureMode(NORMAL);
   opacity = createSlider(0, 1, 0.5, 0.01);
   opacity.position(10, 25);
@@ -31,9 +26,7 @@ function setup() {
 
 function draw() {
   background(200);
-  // reset shader so that the default shader is used to render the 3D scene
   resetShader();
-  // world space scene
   axes();
   grid();
   translate(0, -70);
@@ -43,11 +36,9 @@ function draw() {
   translate(70, 70);
   fill(color(0, 255, 255, 125));
   sphere(30, 50);
-  // use custom shader
   shader(uvShader);
   // https://p5js.org/reference/#/p5.Shader/setUniform
   uvShader.setUniform('opacity', opacity.value());
-  // screen-space quad (i.e., x ∈ [0..width] and y ∈ [0..height])
   // see: https://github.com/VisualComputing/p5.treegl#heads-up-display
   beginHUD();
   noStroke();
@@ -56,6 +47,5 @@ function draw() {
 }
 
 function mouseWheel(event) {
-  //comment to enable page scrolling
   return false;
 }
